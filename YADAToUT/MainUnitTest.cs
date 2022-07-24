6812dfa99da35a92a2c31f3d8bf974f942596eaf
antonio-ferrer <58077592+antonio-ferrer@users.noreply.github.com>
2022-07-24 20:24:09 -0300

    create test project

---
+++ YADAToUT/MainUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text.RegularExpressions;
+
+namespace YADAToUT
+{
+    [TestClass]
+    public class MainUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(Regex.IsMatch("Say my name", "name", RegexOptions.IgnoreCase));
+        }
+    }
+}
